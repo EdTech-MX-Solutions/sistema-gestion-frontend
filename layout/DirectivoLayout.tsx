4dd80f94037918f013a65f28b8e752910db9e239
Rodrigo (Roy) <chavo0022009@gmail.com>
2023-11-11 13:00:25 -0600

    + Directivo | + Profesor Layouts

--- layout/DirectivoLayout.tsx
+++ layout/DirectivoLayout.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import PrivateRoute from "../components/auth/PrivateRoute";
+import SidebarDirectivo from "@/components/Sidebars/SidebarDirectivo";
+import Header from "@/components/Header";
 
 interface LayoutProps {
     children: React.ReactNode;
 }
 
 const TutorLayout = ({ children }: LayoutProps) => {
+    const [sidebarOpen, setSidebarOpen] = useState(false);
     return (
         <>
             <PrivateRoute allowedRoles={["SUPERUSER", "DIRECTIVO"]}>
-                {children}
+                <div className="dark:bg-boxdark-2 dark:text-bodydark">
+                    <div className="flex h-screen overflow-hidden">
+                        <SidebarDirectivo
+                            sidebarOpen={sidebarOpen}
+                            setSidebarOpen={setSidebarOpen}
+                        />
+                        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden w-full ">
+                            <Header
+                                sidebarOpen={sidebarOpen}
+                                setSidebarOpen={setSidebarOpen}
+                            />
+                            <main>
+                                <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                                    {children}
+                                </div>
+                            </main>
+                        </div>
+                    </div>
+                </div>
             </PrivateRoute>
         </>
     );
--- layout/ProfesorLayout.tsx
+++ layout/ProfesorLayout.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import PrivateRoute from "../components/auth/PrivateRoute";
+import SidebarProfesor from "@/components/Sidebars/SidebarProfesor";
+import Header from "@/components/Header";
 
 interface LayoutProps {
     children: React.ReactNode;
 }
 
 const TutorLayout = ({ children }: LayoutProps) => {
+    const [sidebarOpen, setSidebarOpen] = useState(false);
     return (
         <>
             <PrivateRoute allowedRoles={["SUPERUSER", "PROFESOR"]}>
-                {children}
+                <div className="dark:bg-boxdark-2 dark:text-bodydark">
+                    <div className="flex h-screen overflow-hidden">
+                        <SidebarProfesor
+                            sidebarOpen={sidebarOpen}
+                            setSidebarOpen={setSidebarOpen}
+                        />
+                        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden w-full ">
+                            <Header
+                                sidebarOpen={sidebarOpen}
+                                setSidebarOpen={setSidebarOpen}
+                            />
+                            <main>
+                                <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                                    {children}
+                                </div>
+                            </main>
+                        </div>
+                    </div>
+                </div>
             </PrivateRoute>
         </>
     );
