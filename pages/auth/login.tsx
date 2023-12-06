218182bf3bdd70698c0e18c995319c0ffeba27c1
Rodrigo (Roy) <chavo0022009@gmail.com>
2023-12-05 22:06:59 -0600

    * preventDefault

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -51,7 +51,8 @@ export default function SignIn({
                         <div key={provider.name}>
                           <button
                             className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
-                            onClick={() => {
+                            onClick={(e) => {
+                              e.preventDefault();
                               signIn(provider.id);
                             }}
                           >
