1da0afeb128cf469b5084f8264573ad22a2174e6
David Pérez Antonio <david.perez.antonio51@gmail.com>
2023-12-12 22:50:45 -0600

    * Limpieza

--- pages/student/academics/schedule.tsx
+++ pages/student/academics/schedule.tsx
@@ -9,26 +9,6 @@ function Schedule() {
     const { alumnos } = useAlumno();
     const title = "Horario";
     const [alumno,setAlumno] = useState(alumnos[0]);
-    const horario = [
-        {
-            claveMateria: "ESP-2",
-            nombreMateria: "Español",
-            horaLunes: "08:00:00",
-            horaMartes: "11:00:00",
-            horaMiercoles: "12:00:00",
-            horaJueves: "13:00:00",
-            horaViernes: "14:00:00",
-        },
-        {
-            claveMateria: "MAT-2",
-            nombreMateria: "Matematicas",
-            horaLunes: "10:00:00",
-            horaMartes: "11:00:00",
-            horaMiercoles: "12:00:00",
-            horaJueves: "13:00:00",
-            horaViernes: "14:00:00",
-        },
-    ];
 
     return (
         <>
