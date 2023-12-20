0305e8aeb58159b2e5d34f2af9dfe214fc80cee2
Rodrigo (Roy) <chavo0022009@gmail.com>
2023-12-20 03:53:34 -0600

    fix merge

--- pages/professors/grades/panelGrades.tsx
+++ pages/professors/grades/panelGrades.tsx
@@ -1,8 +1,8 @@
 import React, { ReactNode, useState } from "react";
 import PrincipalTitle from "@/components/professor/Principal.Title";
 import TableGrades from "@/components/professor/TableGrades";
-import ButtonComponent from "@/components/ButtonComponent";
 import InterfacePeriodo from "@/data/interfaces/periodo";
+import ButtonComponent from "@/components/elements/Buttons/ButtonComponent";
 
 interface DefaultLayoutProps {
   children?: ReactNode;
